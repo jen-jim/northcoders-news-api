b84a545e3f38603e3fe3d4b9ec0ce5dd57fb2112
Jen Jimenez <44523562+jen-jim@users.noreply.github.com>
2025-01-27 14:19:50 +0000

    setup test suite
    - setup test imports
    - run seed with test-data before each test
    - close the connection to the database after all the tests

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,15 +1,25 @@
+const request = require("supertest");
+const app = require("../app/app");
+const seed = require("../db/seeds/seed");
+const db = require("../db/connection");
+const testData = require("../db/data/test-data/index");
 const endpointsJson = require("../endpoints.json");
-/* Set up your test imports here */
 
-/* Set up your beforeEach & afterAll functions here */
+beforeEach(() => {
+    return seed(testData);
+});
+
+afterAll(() => {
+    return db.end();
+});
 
 describe("GET /api", () => {
-  test("200: Responds with an object detailing the documentation for each endpoint", () => {
-    return request(app)
-      .get("/api")
-      .expect(200)
-      .then(({ body: { endpoints } }) => {
-        expect(endpoints).toEqual(endpointsJson);
-      });
-  });
+    test("200: Responds with an object detailing the documentation for each endpoint", () => {
+        return request(app)
+            .get("/api")
+            .expect(200)
+            .then(({ body: { endpoints } }) => {
+                expect(endpoints).toEqual(endpointsJson);
+            });
+    });
 });
